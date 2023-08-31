21db470ccfe7ac103bb53bdc70fc86efca510878
WiktorPospiech <wiktor.pospiech@gmail.com>
2023-08-31 11:27:14 +0200

    task-01 solution added

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyBackgund = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener('click', () => {
+  startBtn.toggleAttribute('disabled');
+  timerId = setInterval(() => {
+    bodyBackgund.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
